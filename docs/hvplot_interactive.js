c8865969720ed03854377d08d058f3b3b3e621b5
twhesford <85916078+twhesford@users.noreply.github.com>
2023-07-05 14:11:22 -0400

    Add files via upload

--- docs/hvplot_interactive.js
+++ docs/hvplot_interactive.js
@@ -1,4 +1,4 @@
-importScripts("https://cdn.jsdelivr.net/pyodide/v0.21.3/full/pyodide.js");
+importScripts("https://cdn.jsdelivr.net/pyodide/v0.23.0/full/pyodide.js");
 
 function sendPatch(patch, buffers, msg_id) {
   self.postMessage({
@@ -15,14 +15,27 @@ async function startApplication() {
   self.pyodide.globals.set("sendPatch", sendPatch);
   console.log("Loaded!");
   await self.pyodide.loadPackage("micropip");
-  const env_spec = ['https://cdn.holoviz.org/panel/0.14.0/dist/wheels/bokeh-2.4.3-py3-none-any.whl', 'https://cdn.holoviz.org/panel/0.14.0/dist/wheels/panel-0.14.0-py3-none-any.whl', 'holoviews>=1.15.1', 'holoviews>=1.15.1', 'hvplot', 'pandas']
+  const env_spec = ['markdown-it-py<3', 'https://cdn.holoviz.org/panel/1.1.1/dist/wheels/bokeh-3.1.1-py3-none-any.whl', 'https://cdn.holoviz.org/panel/1.1.1/dist/wheels/panel-1.1.1-py3-none-any.whl', 'pyodide-http==0.2.1', 'holoviews', 'hvplot', 'pandas']
   for (const pkg of env_spec) {
-    const pkg_name = pkg.split('/').slice(-1)[0].split('-')[0]
+    let pkg_name;
+    if (pkg.endsWith('.whl')) {
+      pkg_name = pkg.split('/').slice(-1)[0].split('-')[0]
+    } else {
+      pkg_name = pkg
+    }
     self.postMessage({type: 'status', msg: `Installing ${pkg_name}`})
-    await self.pyodide.runPythonAsync(`
-      import micropip
-      await micropip.install('${pkg}');
-    `);
+    try {
+      await self.pyodide.runPythonAsync(`
+        import micropip
+        await micropip.install('${pkg}');
+      `);
+    } catch(e) {
+      console.log(e)
+      self.postMessage({
+	type: 'status',
+	msg: `Error while installing ${pkg_name}`
+      });
+    }
   }
   console.log("Packages loaded!");
   self.postMessage({type: 'status', msg: 'Executing code'})
@@ -54,13 +67,13 @@ init_doc()
 # 
 # Please note that in **Colab** you will need to \`!pip install panel hvplot\`.
 
-# In[ ]:
+# In[1]:
 
 
 # !pip install panel==0.12.6 hvplot==0.7.3
 
 
-# In[ ]:
+# In[2]:
 
 
 import panel as pn
@@ -68,7 +81,7 @@ import panel as pn
 pn.extension('tabulator', sizing_mode="stretch_width")
 
 
-# In[ ]:
+# In[3]:
 
 
 import hvplot.pandas
@@ -79,7 +92,7 @@ hv.extension('bokeh')
 
 # ## Define function to determine environment
 
-# In[ ]:
+# In[4]:
 
 
 def environment():
@@ -93,7 +106,7 @@ environment()
 
 # ## Define Color Palette
 
-# In[ ]:
+# In[5]:
 
 
 PALETTE = ["#ff6f69", "#ffcc5c", "#88d8b0", ]
@@ -106,7 +119,7 @@ pn.Row(
 
 # ## Load Data
 
-# In[ ]:
+# In[6]:
 
 
 from bokeh.sampledata.autompg import autompg_clean as df
@@ -115,7 +128,7 @@ df.head()
 
 # ## Define DataFrame Pipeline
 
-# In[ ]:
+# In[7]:
 
 
 (
@@ -132,7 +145,7 @@ df.head()
 
 # ## Make DataFrame Pipeline Interactive
 
-# In[ ]:
+# In[8]:
 
 
 idf = df.interactive()
@@ -140,7 +153,7 @@ idf = df.interactive()
 
 # Define [Panel widgets](https://panel.holoviz.org/reference/index.html#widgets)
 
-# In[ ]:
+# In[9]:
 
 
 cylinders = pn.widgets.IntSlider(name='Cylinders', start=4, end=8, step=2)
@@ -158,7 +171,7 @@ yaxis = pn.widgets.RadioButtonGroup(
 
 # Combine pipeline and widgets
 
-# In[ ]:
+# In[10]:
 
 
 ipipeline = (
@@ -177,7 +190,7 @@ ipipeline.head()
 
 # ## Pipe to Table
 
-# In[ ]:
+# In[11]:
 
 
 if environment()=="server":
@@ -186,7 +199,7 @@ else:
    theme="simple"
 
 
-# In[ ]:
+# In[12]:
 
 
 itable = ipipeline.pipe(pn.widgets.Tabulator, pagination='remote', page_size=10)
@@ -197,7 +210,7 @@ itable
 
 # ## Pipe to hvplot
 
-# In[ ]:
+# In[13]:
 
 
 ihvplot = ipipeline.hvplot(x='mpg', y=yaxis, by='origin', color=PALETTE, line_width=6, height=400)
@@ -208,7 +221,7 @@ ihvplot
 # 
 # Here we use the [FastListTemplate](https://panel.holoviz.org/reference/templates/FastListTemplate.html#templates-gallery-fastlisttemplate).
 
-# In[ ]:
+# In[15]:
 
 
 template = pn.template.FastListTemplate(
@@ -229,13 +242,24 @@ template.servable();
 
 await write_doc()
   `
-  const [docs_json, render_items, root_ids] = await self.pyodide.runPythonAsync(code)
-  self.postMessage({
-    type: 'render',
-    docs_json: docs_json,
-    render_items: render_items,
-    root_ids: root_ids
-  });
+
+  try {
+    const [docs_json, render_items, root_ids] = await self.pyodide.runPythonAsync(code)
+    self.postMessage({
+      type: 'render',
+      docs_json: docs_json,
+      render_items: render_items,
+      root_ids: root_ids
+    })
+  } catch(e) {
+    const traceback = `${e}`
+    const tblines = traceback.split('\n')
+    self.postMessage({
+      type: 'status',
+      msg: tblines[tblines.length-2]
+    });
+    throw e
+  }
 }
 
 self.onmessage = async (event) => {
@@ -248,19 +272,19 @@ self.onmessage = async (event) => {
     _link_docs_worker(state.curdoc, sendPatch, setter='js')
     `)
   } else if (msg.type === 'patch') {
+    self.pyodide.globals.set('patch', msg.patch)
     self.pyodide.runPythonAsync(`
-    import json
-
-    state.curdoc.apply_json_patch(json.loads('${msg.patch}'), setter='js')
+    state.curdoc.apply_json_patch(patch.to_py(), setter='js')
     `)
     self.postMessage({type: 'idle'})
   } else if (msg.type === 'location') {
+    self.pyodide.globals.set('location', msg.location)
     self.pyodide.runPythonAsync(`
     import json
     from panel.io.state import state
     from panel.util import edit_readonly
     if state.location:
-        loc_data = json.loads("""${msg.location}""")
+        loc_data = json.loads(location)
         with edit_readonly(state.location):
             state.location.param.update({
                 k: v for k, v in loc_data.items() if k in state.location.param
